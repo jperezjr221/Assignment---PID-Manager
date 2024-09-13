b823037e6ca03a32f4d516ed514c26b02ed37664
Juan Perez <jperezjr@csu.fullerton.edu>
2024-09-12 21:27:01 -0700

    template.cpp

--- saturdayy.cpp
+++ saturdayy.cpp
@@ -1,26 +1,11 @@
-#include <sys/wait.h>
-#include <sys/types.h>
+// Juan Perez
+// jperezjr@csu.fullerton.edu
+// @jperezjr221
+// Partners: 
+
 #include <iostream>
-#include <unistd.h>
 
-int main() {
-    pid_t pid;
-
-    // Fork a child process
-    pid = fork();
-
-    if (pid < 0) { // Error occurred
-        std::cerr << "Fork Failed" << std::endl;
-        return 1;
-    }
-    else if (pid == 0) { // Child process
-        execlp("/bin/ls", "ls", NULL);
-    }
-    else { // Parent process
-        // Parent will wait for the child to complete
-        wait(NULL);
-        std::cout << "Child Complete" << std::endl;
-    }
-
-    return 0;
+int main(int argc, const char *argv[]) {
+  std::cout << "Hello YourName!\n";
+  return 0;
 }
